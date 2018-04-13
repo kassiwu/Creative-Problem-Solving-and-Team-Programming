185043ea4f80fe39cfdf6a98d5bbc24ba8c7b6b2
Dylan DiGeronimo <ddigeron@stevens.edu>
2018-04-13 01:09:28 -0400

    Started final project

---
+++ arrayAndSimpleQueries/arrayAndSimpleQueries.cpp
@@ -0,0 +1,19 @@
+/*
+ *  arrayAndSimpleQueries.cpp
+ *
+ *  Created on: Apr 12, 2018
+ *  Author: Dylan DiGeronimo, Jacob Fallin, Kexian Wu
+ *  I pledge my honor that I have abided by the Stevens Honor System.
+ *  Problem: https://www.hackerrank.com/challenges/array-and-simple-queries/problem
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+}
